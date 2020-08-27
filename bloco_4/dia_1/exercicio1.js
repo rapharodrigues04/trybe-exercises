ccc87ee3cf43ba97a6cbffcb583b9db85e118003
Raphael Rodrigues <rapha.rodrigues04@gmail.com>
2020-08-27 17:47:52 -0300

    Concluído o primeiro exercicio sobre JS

---
+++ bloco_4/dia_1/exercicio1.js
@@ -0,0 +1,23 @@
+let a = 10;
+let b = 20;
+
+//Adição:
+let adicao = a + b;
+console.log(`O resultado da adição é ${adicao}.`); 
+
+//Subtracão:
+let subtracao = a - b;
+console.log(`O resultado da subtração é ${subtracao}.`); 
+
+//Multiplicação:
+let multiplicacao = a * b;
+console.log(`O resultado da multiplicação é ${multiplicacao}.`); 
+
+//Divisão:
+let divisao = a / b;
+console.log(`O resultado da divisão é ${divisao}.`); 
+
+
+//Módulo:
+let modulo = a % b;
+console.log(`O resultado do módulo é ${modulo}.`); 
