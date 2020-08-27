99a611ea6cd2b2799fdf44ca6d7018ee52927a5e
Raphael Rodrigues <rapha.rodrigues04@gmail.com>
2020-08-27 18:40:41 -0300

    Concluído o quinto exercicio sobre JS

---
+++ bloco_4/dia_1/exercicio5.js
@@ -0,0 +1,11 @@
+let angulo_1 = 20;
+let angulo_2 = 50;
+let angulo_3 = 110;
+
+let areaTriangulo = 180;
+
+if(angulo_1 + angulo_2 + angulo_3 == areaTriangulo){
+    return true;
+} else {
+    console.log('Ângulos não são se um triângulo válido!');
+}
