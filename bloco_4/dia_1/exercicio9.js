5c05536ac4431e1ff27e474c49cb779dca8a4a8b
Raphael Rodrigues <rapha.rodrigues04@gmail.com>
2020-08-28 17:58:11 -0300

    Concluído o nono exercicio sobre JS

---
+++ bloco_4/dia_1/exercicio9.js
@@ -0,0 +1,9 @@
+let numero1 = 10;
+let numero2 = 30;
+let numero3 = 41;
+
+if (numero1 % 2 !== 0 || numero2 % 2 !== 0 || numero3 % 2 !== 0) {
+    console.log(true)
+} else {
+    console.log(false);
+}
